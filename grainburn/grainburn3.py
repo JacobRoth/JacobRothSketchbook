1f2aebf80c1b07e745aca520ddfcc854ceffccf6
Jacob Roth <rothj1459@gmail.com>
2013-10-31 08:40:04 -0600

    added edge-detecting pointsList creating subroutine.

--- grainburn/grainburn3.py
+++ grainburn/grainburn3.py
@@ -21,6 +21,45 @@ def populatePointsListDumb(pointsList, image): # dumb version, no edge detection
         for column in range(0,columns-1):
             if image[row][column] == 1: #nitrous, add an expandcircle here
                 pointsList.append( (row,column) )
+
+def populatePointsListSmart(pointsList, image): #adds edge detection, making the pointsList much shorter but still definitive
+    #remember, in the image 1 is nitrous/bore, and 0 is fuel
+    rows,columns = image.shape
+    for row in range(0,rows-1):
+        for column in range(0,columns-1):
+            if image[row][column] == 1: #nitrous
+                addCircle = False
+
+                #these are all run with try: statements because sometimes we will attempt to analyze a square that does not exist. 
+                try:
+                    if image[row][column-1] == 0: #touching a fuel square, so we are on the edge
+                        addCircle = True
+                except:
+                    pass #dont bother, just move on to analyzing the next square
+
+                try:
+                    if image[row][column+1] == 0: #touching a fuel square, so we are on the edge
+                        addCircle = True
+                except:
+                    pass #dont bother, just move on to analyzing the next square
+                
+                try:
+                    if image[row-1][column] == 0: #touching a fuel square, so we are on the edge
+                        addCircle = True
+                except:
+                    pass #dont bother, just move on to analyzing the next square
+
+                try:
+                    if image[row+1][column] == 0: #touching a fuel square, so we are on the edge
+                        addCircle = True
+                except:
+                    pass #dont bother, just move on to analyzing the next square
+                        
+                if addCircle == True:
+                    pointsList.append( (row,column) )
+
+#def debugColorPoints(pointsList, image):
+    #for 
             
 
 def main():
@@ -30,15 +69,17 @@ def main():
     
     pointsList = [] # indexed as row,column (backwards) because that's how MATPLOTLIB rolls
     fuelgrain = mpimg.imread('tinergrain.png')
-
-    populatePointsListDumb(pointsList,fuelgrain)
     
     size,scratch = fuelgrain.shape
     if size != scratch:
         print("error error - that image is not square. I can't work with that.")
         print("please have image width equal image height exactly")
+        return "err - nonsquare image"
     
     imgplot=plt.imshow(fuelgrain)
+    imgplot.set_cmap("gray") # sets the correct color scheme
+    plt.show()
+    populatePointsListSmart(pointsList ,fuelgrain)
 
 if __name__ == "__main__":
     main()
