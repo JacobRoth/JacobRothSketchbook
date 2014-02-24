2adaab938c727003f6f9684d3280349a0533ba8f
Jacob Roth <rothj1459@gmail.com>
2014-02-24 14:41:40 -0600

    still working on stuff. I wrote a good description of what I want the client to do.

--- sockets/client.py
+++ sockets/client.py
@@ -3,6 +3,28 @@ import socket,os
 HOST = 'localhost'
 PORT = 4325
 
+# the plan here is that this will be a dumb untrusted client that does this:
+
+# print MOTD/instructions
+# prompt for login or register
+# if register:
+#    get name, password*s*, textual captcha if that's a thing
+#    send_text("register "+name+" "+str(hash(pass))
+#    print( "you should be registered now, 
+
+
+# if login:
+#    prompt for name and pass, STORE THEM LOCALLY (VARS IN THIS SCRIPT)
+#    more specifically, hash the password
+#    send name+hash(pass)+"checklogin"
+#    server returns if you're logged in.
+#    while (True):
+#       promt command
+#       send name+hash(pass)+command
+#       print response
+
+# there will be no "logins"
+
 def send_text(text): #returns the response from the server
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST,PORT))
@@ -16,6 +38,8 @@ def send_text(text): #returns the response from the server
     print(data.decode('UTF-8'))
     
     s.close()
+
+
 
 while (True):
     send_text(input("? "))
--- sockets/server.py
+++ sockets/server.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-# server that creates a system user
-# just to test
+# server 
 
-import socket,os
+import socket,os,threading,multiprocessing
 
 HOST = 'localhost'                 # Symbolic name meaning all available interfaces
 PORT = 4325              # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(5)
-while True:
-    conn, addr = s.accept()
+CPUS = multiprocessing.cpu_count()
+
+def do_receive(conn,addr):
     print('Connected by', addr)
     data = None
     while not data:                #wait until we get something
@@ -19,3 +16,15 @@ while True:
     conn.send(b'some response bytes')
     
     conn.close()
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(5)
+    while True:
+        conn, addr = s.accept()
+        do_receive(conn,addr)
+
+if __name__ == "__main__":
+    main()
+        
